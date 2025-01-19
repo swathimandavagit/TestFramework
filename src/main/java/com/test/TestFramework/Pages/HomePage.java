6e10877dd050f8893c2cd927554f78f37058a7b3
abhinav-neo <mandava.swathi@gmail.com>
2025-01-18 18:07:52 -0600

    Removing invalid

--- src/main/java/com/test/TestFramework/Pages/HomePage.java
+++ src/main/java/com/test/TestFramework/Pages/HomePage.java
@@ -1,5 +1,7 @@
 package com.test.TestFramework.Pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
@@ -9,9 +11,10 @@ import org.testng.Assert;
 import com.test.TestFramework.Common.CommonTest;
 
 
-
 public class HomePage extends CommonTest {
 
+	@FindBy(how=How.XPATH, using="//button[@id='onetrust-accept-btn-handler']")
+	private WebElement cookieAccept;
 	@FindBy(how=How.XPATH, using="//button[contains(text(),'Rail Passes')]")
 	private WebElement railPassesButton;
 	@FindBy(xpath="//span[text()='Amtrak Multi-Ride & Rail Passes']")
@@ -23,6 +26,8 @@ public class HomePage extends CommonTest {
 	public void navigateToWebsite() {		
 			driver.get("https://www.amtrak.com/home.html");
 			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		    cookieAccept.click();
 	}
 	
 	public void clickOnRailPasses() {
--- target/test-classes/com/test/TestFramework/Features/TestFeature1.feature
+++
@@ -1,8 +0,0 @@
-Feature: Test feature
-  
-  
-  Scenario: This is a sample test
-  Given I navigate to the website
-  When I click on RailPasses option
-  Then I should land in Rail passes page
-  
